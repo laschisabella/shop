e999d3078d2f58170e4f30e8a5d04e74c19c8d95
Isabella Laschi <laschi.isabella@gmail.com>
2023-02-24 10:28:08 -0300

    first commit

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">shop</a>
+      </header>
+      <main>
+        list of products
+      </main>
+    </div>
+  );
+}
